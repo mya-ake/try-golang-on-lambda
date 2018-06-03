f56e74e72eb5d683ece90cea6f2678f274b467d3
mya-ake <make.app.ts@gmail.com>
2018-06-04 01:39:29 +0900

    API Gateway で JSON データを返してみた

--- hello/main.go
+++ hello/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
@@ -8,9 +12,19 @@ type Response struct {
 	Message string `json:"message"`
 }
 
-func Handler() (Response, error) {
-	return Response{
+func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	fmt.Println("Received body: ", request.Body)
+
+	response := Response{
 		Message: "Go Serverless v1.0! Your function executed successfully!",
+	}
+	bytes, err := json.Marshal(response)
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
+	return events.APIGatewayProxyResponse{
+		Body:       string(bytes[:]),
+		StatusCode: 200,
 	}, nil
 }
 
